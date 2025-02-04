4ff3924554d7ec32d01376fb93c44f4f258f3c94
torres-74 <penrico@t-online.de>
2025-02-04 09:41:05 +0100

    Update SoftwareUpdate.py

--- lib/python/Screens/SoftwareUpdate.py
+++ lib/python/Screens/SoftwareUpdate.py
@@ -118,15 +118,15 @@ class UpdatePlugin(Screen, ProtectedScreen):
 					elif 'en_EN' in message:
 						message = message['en_EN']
 					else:
-						message = _("The current image might not be stable.\nFor more information see %s.") % ("https://forums.cobraliberosat.net")
+						message = _("The current image might not be stable.\nFor more information see %s.") % ("https://forums.openpli.org")
 
 			except Exception as er:
 				print("[UpdatePlugin] status error", er)
-				message = _("The current image might not be stable.\nFor more information see %s.") % ("https://forums.cobraliberosat.net")
+				message = _("The current image might not be stable.\nFor more information see %s.") % ("https://forums.openpli.org")
 
 		# or display a generic warning if fetching failed
 		else:
-			message = _("The status of the current image could not be checked because %s can not be reached.") % ("https://cobraliberosat.net")
+			message = _("The status of the current image could not be checked because %s can not be reached.") % ("https://openpli.org")
 
 		# show the user the message first
 		if message is not None:
